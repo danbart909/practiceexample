eb5831ff8816ee2cce6844696a7e2a016dc9cca1
danbart909 <53535966+danbart909@users.noreply.github.com>
2019-12-07 14:04:57 -0500

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './Card.css';
-import STORE from './STORE';
 
-function Card(props) {
-	return(
-		<div classname="Card">
-  		<button type="button">delete</button>
-  		<h3>{props.title}</h3>
-  		<p>{props.content}</p>
-		</div>
-	)
+export default function Card(props) {
+  return (
+    <div className='Card'>
+      <button
+        type='button'
+      >
+        delete
+      </button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
 }
-
-export default Card;
